66c2ac1a26d0efb879a223341a71986f1ebd08b8
SamehElhady <100520255+SamehElhady@users.noreply.github.com>
2023-05-24 21:49:54 +0200

    finish css miss javascript

---
+++ app.js
@@ -0,0 +1,19 @@
+const input=document.querySelectorAll(".input");
+const submit=document.getElementById("submit");
+
+
+submit.addEventListener("click",function(event){
+    input.forEach(function(inpu){
+        
+        
+    })
+})
+
+
+
+const warrn=new Image(10,10);
+
+
+console.log(input);
+
+//input[0].value=warrn
